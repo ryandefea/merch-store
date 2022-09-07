d2b42650ca6498d98d642f3a1a0ad3a1362f6817
ryandefea <ryan.defea@gmail.com>
2022-09-07 17:29:28 -0500

    create inventory
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Merch(props){
+function Inventory(props){
   return (
     <React.Fragment>
       <h2>{props.name}</h2>
       <h2>{props.price}</h2>
       <h2>{props.size}</h2>
       <h2>{props.description}</h2>
+      
     </React.Fragment>
   );
 }
 
 
-Merch.propTypes = {
+Inventory.propTypes = {
   size: PropTypes.string,
   price: PropTypes.number,
   description: PropTypes.string,
   name: PropTypes.string
 };
 
-export default Merch;
+export default Inventory;
